02db7c8f399568d1a2df914b3bdc0b769da11ab2
Matt Champion <mattunderscorechampion@gmail.com>
2016-07-14 09:49:28 +0100

    Move the entry point for the adapter into a separate method.

---
+++ adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/RESTAdapter.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.client;
+
+import static java.util.Collections.singletonList;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import com.pushtechnology.adapters.rest.model.conversion.ConversionContext;
+import com.pushtechnology.adapters.rest.model.conversion.LatestConverter;
+import com.pushtechnology.adapters.rest.model.conversion.V0Converter;
+import com.pushtechnology.adapters.rest.model.conversion.V1Converter;
+import com.pushtechnology.adapters.rest.model.conversion.V2Converter;
+import com.pushtechnology.adapters.rest.model.conversion.V3Converter;
+import com.pushtechnology.adapters.rest.model.conversion.V4Converter;
+import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
+import com.pushtechnology.adapters.rest.model.latest.Model;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+import com.pushtechnology.adapters.rest.persistence.FileSystemPersistence;
+import com.pushtechnology.adapters.rest.persistence.Persistence;
+
+import net.jcip.annotations.Immutable;
+
+/**
+ * Entry point for adapter client from the command line.
+ *
+ * @author Push Technology Limited
+ */
+@Immutable
+public final class RESTAdapter {
+    private RESTAdapter() {
+    }
+
+    /**
+     * Entry point for adapter client.
+     * @param args The command line arguments
+     * @throws IOException if there was a problem reading the persisted configuration
+     * @throws InterruptedException if the thread was interrupted
+     */
+    // CHECKSTYLE.OFF: UncommentedMain
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // CHECKSTYLE.ON: UncommentedMain
+        final ConversionContext conversionContext = ConversionContext
+            .builder()
+            .register(
+                com.pushtechnology.adapters.rest.model.v0.Model.VERSION,
+                com.pushtechnology.adapters.rest.model.v0.Model.class,
+                V0Converter.INSTANCE)
+            .register(
+                com.pushtechnology.adapters.rest.model.v1.Model.VERSION,
+                com.pushtechnology.adapters.rest.model.v1.Model.class,
+                V1Converter.INSTANCE)
+            .register(
+                com.pushtechnology.adapters.rest.model.v2.Model.VERSION,
+                com.pushtechnology.adapters.rest.model.v2.Model.class,
+                V2Converter.INSTANCE)
+            .register(
+                com.pushtechnology.adapters.rest.model.v3.Model.VERSION,
+                com.pushtechnology.adapters.rest.model.v3.Model.class,
+                V3Converter.INSTANCE)
+            .register(
+                com.pushtechnology.adapters.rest.model.v4.Model.VERSION,
+                com.pushtechnology.adapters.rest.model.v4.Model.class,
+                V4Converter.INSTANCE)
+            .register(
+                Model.VERSION,
+                Model.class,
+                LatestConverter.INSTANCE)
+            .build();
+
+        final Persistence fileSystemPersistence = new FileSystemPersistence(Paths.get("."), conversionContext);
+
+        final Optional<Model> config = fileSystemPersistence.loadModel();
+
+        final Model model = config.orElse(
+            Model
+                .builder()
+                .services(
+                    singletonList(
+                        ServiceConfig
+                            .builder()
+                            .host("petition.parliament.uk")
+                            .port(80)
+                            .endpoints(
+                                singletonList(
+                                    EndpointConfig
+                                        .builder()
+                                        .name("endpoint-0")
+                                        .url("/petitions/131215.json")
+                                        .topic("petitions/131215")
+                                        .build()))
+                            .build()))
+                .build());
+
+        fileSystemPersistence.storeModel(model);
+
+        final RESTAdapterClient adapterClient = RESTAdapterClient.create(model);
+
+        adapterClient.start();
+    }
+}
--- adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/RESTAdapterClient.java
+++ adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/RESTAdapterClient.java
@@ -16,30 +16,18 @@
 package com.pushtechnology.adapters.rest.client;
 
 import static com.pushtechnology.diffusion.client.session.SessionAttributes.Transport.WEBSOCKET;
-import static java.util.Collections.singletonList;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.pushtechnology.adapters.rest.model.conversion.ConversionContext;
-import com.pushtechnology.adapters.rest.model.conversion.LatestConverter;
-import com.pushtechnology.adapters.rest.model.conversion.V0Converter;
-import com.pushtechnology.adapters.rest.model.conversion.V1Converter;
-import com.pushtechnology.adapters.rest.model.conversion.V2Converter;
-import com.pushtechnology.adapters.rest.model.conversion.V3Converter;
-import com.pushtechnology.adapters.rest.model.conversion.V4Converter;
 import com.pushtechnology.adapters.rest.model.latest.DiffusionConfig;
 import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
 import com.pushtechnology.adapters.rest.model.latest.Model;
 import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
-import com.pushtechnology.adapters.rest.persistence.FileSystemPersistence;
-import com.pushtechnology.adapters.rest.persistence.Persistence;
 import com.pushtechnology.adapters.rest.polling.HttpClientFactoryImpl;
 import com.pushtechnology.adapters.rest.polling.PollClient;
 import com.pushtechnology.adapters.rest.polling.PollClientImpl;
@@ -50,51 +38,55 @@ import com.pushtechnology.adapters.rest.publication.PublishingClientImpl;
 import com.pushtechnology.diffusion.client.Diffusion;
 import com.pushtechnology.diffusion.client.session.SessionFactory;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 /**
  * Simple client adapting REST to Diffusion.
  *
  * @author Push Technology Limited
  */
+@ThreadSafe
 public final class RESTAdapterClient {
     private static final Logger LOG = LoggerFactory.getLogger(RESTAdapterClient.class);
 
+    private final Model model;
     private final PublishingClient publishingClient;
     private final PollClient pollClient;
-    private final ScheduledExecutorService executor;
+    @GuardedBy("this")
+    private ScheduledExecutorService currentExecutor;
 
     private RESTAdapterClient(
-        PublishingClient publishingClient,
-        PollClient pollClient,
-        ScheduledExecutorService executor) {
+            Model model,
+            PublishingClient publishingClient,
+            PollClient pollClient) {
 
+        this.model = model;
         this.publishingClient = publishingClient;
         this.pollClient = pollClient;
-        this.executor = executor;
     }
 
     /**
-     * Factory method for {@link RESTAdapterClient}.
-     * @param model the configuration to use
-     * @return a new {@link RESTAdapterClient}
+     * Start the client.
+     * @throws IllegalStateException if the client is running
      */
-    public static RESTAdapterClient create(Model model) {
-        LOG.debug("Creating REST adapter client with configuration: {}", model);
-        final PublishingClient diffusionClient = new PublishingClientImpl(getSessionFactory(model.getDiffusion()));
+    public synchronized void start() {
+        if (currentExecutor != null) {
+            throw new IllegalStateException("The client is already running");
+        }
 
-        diffusionClient.start();
-
-        final PollClient pollClient = new PollClientImpl(new HttpClientFactoryImpl());
+        publishingClient.start();
         pollClient.start();
 
         final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
         model
             .getServices()
-            .forEach(service -> diffusionClient.initialise(
+            .forEach(service -> publishingClient.initialise(
                 service,
                 new InitialiseCallback() {
                     private final ServiceSession serviceSession =
-                        new ServiceSession(executor, pollClient, service, diffusionClient);
+                        new ServiceSession(executor, pollClient, service, publishingClient);
 
                     @Override
                     public void onEndpointAdded(ServiceConfig serviceConfig, EndpointConfig endpointConfig) {
@@ -110,74 +102,34 @@ public final class RESTAdapterClient {
                         serviceSession.start();
                     }
                 }));
-
-        return new RESTAdapterClient(diffusionClient, pollClient, executor);
     }
 
     /**
-     * Entry point for adapter client.
-     * @param args The command line arguments
-     * @throws IOException if there was a problem reading the persisted configuration
-     * @throws InterruptedException if the thread was interrupted
+     * Stop the client.
+     * @throws IllegalStateException if the client is not running
      */
-    // CHECKSTYLE.OFF: UncommentedMain
-    public static void main(String[] args) throws IOException, InterruptedException {
-    // CHECKSTYLE.ON: UncommentedMain
-        final ConversionContext conversionContext = ConversionContext
-            .builder()
-            .register(
-                com.pushtechnology.adapters.rest.model.v0.Model.VERSION,
-                com.pushtechnology.adapters.rest.model.v0.Model.class,
-                V0Converter.INSTANCE)
-            .register(
-                com.pushtechnology.adapters.rest.model.v1.Model.VERSION,
-                com.pushtechnology.adapters.rest.model.v1.Model.class,
-                V1Converter.INSTANCE)
-            .register(
-                com.pushtechnology.adapters.rest.model.v2.Model.VERSION,
-                com.pushtechnology.adapters.rest.model.v2.Model.class,
-                V2Converter.INSTANCE)
-            .register(
-                com.pushtechnology.adapters.rest.model.v3.Model.VERSION,
-                com.pushtechnology.adapters.rest.model.v3.Model.class,
-                V3Converter.INSTANCE)
-            .register(
-                com.pushtechnology.adapters.rest.model.v4.Model.VERSION,
-                com.pushtechnology.adapters.rest.model.v4.Model.class,
-                V4Converter.INSTANCE)
-            .register(
-                Model.VERSION,
-                Model.class,
-                LatestConverter.INSTANCE)
-            .build();
+    public synchronized void stop() throws IOException {
+        if (currentExecutor == null) {
+            throw new IllegalStateException("The client is not running");
+        }
 
-        final Persistence fileSystemPersistence = new FileSystemPersistence(Paths.get("."), conversionContext);
+        publishingClient.stop();
+        pollClient.stop();
+        currentExecutor.shutdown();
+        currentExecutor = null;
+    }
 
-        final Optional<Model> config = fileSystemPersistence.loadModel();
+    /**
+     * Factory method for {@link RESTAdapterClient}.
+     * @param model the configuration to use
+     * @return a new {@link RESTAdapterClient}
+     */
+    public static RESTAdapterClient create(Model model) {
+        LOG.debug("Creating REST adapter client with configuration: {}", model);
+        final PublishingClient diffusionClient = new PublishingClientImpl(getSessionFactory(model.getDiffusion()));
+        final PollClient pollClient = new PollClientImpl(new HttpClientFactoryImpl());
 
-        final Model model = config.orElse(
-            Model
-                .builder()
-                .services(
-                    singletonList(
-                        ServiceConfig
-                            .builder()
-                            .host("petition.parliament.uk")
-                            .port(80)
-                            .endpoints(
-                                singletonList(
-                                    EndpointConfig
-                                        .builder()
-                                        .name("endpoint-0")
-                                        .url("/petitions/131215.json")
-                                        .topic("petitions/131215")
-                                        .build()))
-                            .build()))
-                .build());
-
-        fileSystemPersistence.storeModel(model);
-
-        create(model);
+        return new RESTAdapterClient(model, diffusionClient, pollClient);
     }
 
     private static SessionFactory getSessionFactory(DiffusionConfig diffusionConfig) {
