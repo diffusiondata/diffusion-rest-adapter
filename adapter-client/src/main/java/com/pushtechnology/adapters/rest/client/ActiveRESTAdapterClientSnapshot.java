4a16d2bb24ee10d4976e731320863e72187da2c1
Matt Champion <mattunderscorechampion@gmail.com>
2016-07-19 12:31:30 +0100

    Move the creation of the ActiveRESTAdapterClientSnapshot into a factory method.

--- adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/ActiveRESTAdapterClientSnapshot.java
+++ adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/ActiveRESTAdapterClientSnapshot.java
@@ -15,11 +15,30 @@
 
 package com.pushtechnology.adapters.rest.client;
 
+import static com.pushtechnology.diffusion.client.session.SessionAttributes.Transport.WEBSOCKET;
+
 import java.io.IOException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pushtechnology.adapters.rest.model.latest.DiffusionConfig;
+import com.pushtechnology.adapters.rest.model.latest.Model;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+import com.pushtechnology.adapters.rest.polling.PollClient;
+import com.pushtechnology.adapters.rest.polling.PollHandlerFactory;
+import com.pushtechnology.adapters.rest.polling.ServiceSession;
+import com.pushtechnology.adapters.rest.polling.ServiceSessionImpl;
+import com.pushtechnology.adapters.rest.publication.PublishingClient;
+import com.pushtechnology.adapters.rest.publication.PublishingClientImpl;
+import com.pushtechnology.adapters.rest.topic.management.TopicManagementClient;
+import com.pushtechnology.adapters.rest.topic.management.TopicManagementClientImpl;
+import com.pushtechnology.diffusion.client.Diffusion;
 import com.pushtechnology.diffusion.client.session.Session;
+import com.pushtechnology.diffusion.client.session.SessionFactory;
 
 import net.jcip.annotations.ThreadSafe;
 
@@ -30,11 +49,12 @@ import net.jcip.annotations.ThreadSafe;
  */
 @ThreadSafe
 /*package*/ final class ActiveRESTAdapterClientSnapshot implements RESTAdapterClientSnapshot {
+    private static final Logger LOG = LoggerFactory.getLogger(ActiveRESTAdapterClientSnapshot.class);
     private final ScheduledExecutorService currentExecutor;
     private final Session session;
     private final AtomicBoolean isActive;
 
-    /*package*/ ActiveRESTAdapterClientSnapshot(
+    private ActiveRESTAdapterClientSnapshot(
         ScheduledExecutorService currentExecutor,
         Session session,
         AtomicBoolean isActive) {
@@ -49,5 +69,82 @@ import net.jcip.annotations.ThreadSafe;
         isActive.set(false);
         currentExecutor.shutdown();
         session.close();
+    }
+
+    /**
+     * @return a new snapshot
+     */
+    /*package*/ static RESTAdapterClientSnapshot create(Model model, PollClient pollClient, RESTAdapterClient client) {
+        final AtomicBoolean isActive = new AtomicBoolean(true);
+        final Session session = getSession(model.getDiffusion(), isActive, client);
+        final TopicManagementClient topicManagementClient = new TopicManagementClientImpl(session);
+        final PublishingClient publishingClient = new PublishingClientImpl(session);
+
+        final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+        final PollHandlerFactory handlerFactory = (serviceConfig, endpointConfig) -> new PollPublishingHandler(
+            publishingClient, serviceConfig, endpointConfig);
+
+        for (ServiceConfig service : model.getServices()) {
+            final ServiceSession serviceSession = new ServiceSessionImpl(executor, pollClient, service, handlerFactory);
+            topicManagementClient.addService(service);
+            publishingClient
+                .addService(service)
+                .thenAccept(new ServiceReadyForPublishing(topicManagementClient, serviceSession));
+        }
+
+        return new ActiveRESTAdapterClientSnapshot(executor, session, isActive);
+    }
+
+    private static Session getSession(
+        DiffusionConfig diffusionConfig,
+        AtomicBoolean isActive,
+        RESTAdapterClient client) {
+
+        final SessionFactory sessionFactory = Diffusion
+            .sessions()
+            .serverHost(diffusionConfig.getHost())
+            .serverPort(diffusionConfig.getPort())
+            .secureTransport(false)
+            .transports(WEBSOCKET)
+            .reconnectionTimeout(5000)
+            .listener(new Listener(isActive, client));
+
+        if (diffusionConfig.getPrincipal() != null && diffusionConfig.getPassword() != null) {
+            return sessionFactory
+                .principal(diffusionConfig.getPrincipal())
+                .password(diffusionConfig.getPassword())
+                .open();
+        }
+        else  {
+            return sessionFactory.open();
+        }
+    }
+
+    /**
+     * A {@link Session.Listener} to handle session closes.
+     * <p>
+     * If the session closes when the state is active the connection and recovery must have failed, stop the client.
+     */
+    private static final class Listener implements Session.Listener {
+        private final AtomicBoolean isActive;
+        private final RESTAdapterClient client;
+
+        public Listener(AtomicBoolean isActive, RESTAdapterClient client) {
+            this.isActive = isActive;
+            this.client = client;
+        }
+
+        @Override
+        public void onSessionStateChanged(Session forSession, Session.State oldState, Session.State newState) {
+            if (isActive.get() && newState.isClosed()) {
+                try {
+                    client.stop();
+                }
+                catch (IOException e) {
+                    LOG.warn("Exception stopping client", e);
+                }
+            }
+        }
     }
 }
--- adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/RESTAdapterClientSnapshotFactory.java
+++ adapter-client/src/main/java/com/pushtechnology/adapters/rest/client/RESTAdapterClientSnapshotFactory.java
@@ -15,33 +15,15 @@
 
 package com.pushtechnology.adapters.rest.client;
 
-import static com.pushtechnology.diffusion.client.session.SessionAttributes.Transport.WEBSOCKET;
 import static java.util.stream.Collectors.counting;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.pushtechnology.adapters.rest.model.latest.DiffusionConfig;
 import com.pushtechnology.adapters.rest.model.latest.Model;
 import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
 import com.pushtechnology.adapters.rest.polling.PollClient;
-import com.pushtechnology.adapters.rest.polling.PollHandlerFactory;
-import com.pushtechnology.adapters.rest.polling.ServiceSession;
-import com.pushtechnology.adapters.rest.polling.ServiceSessionImpl;
-import com.pushtechnology.adapters.rest.publication.PublishingClient;
-import com.pushtechnology.adapters.rest.publication.PublishingClientImpl;
-import com.pushtechnology.adapters.rest.topic.management.TopicManagementClient;
-import com.pushtechnology.adapters.rest.topic.management.TopicManagementClientImpl;
-import com.pushtechnology.diffusion.client.Diffusion;
-import com.pushtechnology.diffusion.client.session.Session;
-import com.pushtechnology.diffusion.client.session.SessionFactory;
 
 /**
  * For snapshots of the {@link RESTAdapterClient} for a configuration model.
@@ -49,8 +31,6 @@ import com.pushtechnology.diffusion.client.session.SessionFactory;
  * @author Push Technology Limited
  */
 public final class RESTAdapterClientSnapshotFactory {
-    private static final Logger LOG = LoggerFactory.getLogger(RESTAdapterClientSnapshotFactory.class);
-
     /**
      * @return a new snapshot
      */
@@ -69,77 +49,6 @@ public final class RESTAdapterClientSnapshotFactory {
             return InactiveRESTAdapterClientSnapshot.INSTANCE;
         }
 
-        final AtomicBoolean isActive = new AtomicBoolean(true);
-        final Session session = getSession(diffusionConfig, isActive, client);
-        final TopicManagementClient topicManagementClient = new TopicManagementClientImpl(session);
-        final PublishingClient publishingClient = new PublishingClientImpl(session);
-
-        final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
-        final PollHandlerFactory handlerFactory = (serviceConfig, endpointConfig) -> new PollPublishingHandler(
-            publishingClient, serviceConfig, endpointConfig);
-
-        for (ServiceConfig service : services) {
-            final ServiceSession serviceSession = new ServiceSessionImpl(executor, pollClient, service, handlerFactory);
-            topicManagementClient.addService(service);
-            publishingClient
-                .addService(service)
-                .thenAccept(new ServiceReadyForPublishing(topicManagementClient, serviceSession));
-        }
-
-        return new ActiveRESTAdapterClientSnapshot(executor, session, isActive);
+        return ActiveRESTAdapterClientSnapshot.create(model, pollClient, client);
     }
-
-    private static Session getSession(
-        DiffusionConfig diffusionConfig,
-        AtomicBoolean isActive,
-        RESTAdapterClient client) {
-
-        final SessionFactory sessionFactory = Diffusion
-            .sessions()
-            .serverHost(diffusionConfig.getHost())
-            .serverPort(diffusionConfig.getPort())
-            .secureTransport(false)
-            .transports(WEBSOCKET)
-            .reconnectionTimeout(5000)
-            .listener(new Listener(isActive, client));
-
-        if (diffusionConfig.getPrincipal() != null && diffusionConfig.getPassword() != null) {
-            return sessionFactory
-                .principal(diffusionConfig.getPrincipal())
-                .password(diffusionConfig.getPassword())
-                .open();
-        }
-        else  {
-            return sessionFactory.open();
-        }
-    }
-
-    /**
-     * A {@link Session.Listener} to handle session closes.
-     * <p>
-     * If the session closes when the state is active the connection and recovery must have failed, stop the client.
-     */
-    private static final class Listener implements Session.Listener {
-        private final AtomicBoolean isActive;
-        private final RESTAdapterClient client;
-
-        public Listener(AtomicBoolean isActive, RESTAdapterClient client) {
-            this.isActive = isActive;
-            this.client = client;
-        }
-
-        @Override
-        public void onSessionStateChanged(Session forSession, Session.State oldState, Session.State newState) {
-            if (isActive.get() && newState.isClosed()) {
-                try {
-                    client.stop();
-                }
-                catch (IOException e) {
-                    LOG.warn("Exception stopping client", e);
-                }
-            }
-        }
-    }
-
 }
