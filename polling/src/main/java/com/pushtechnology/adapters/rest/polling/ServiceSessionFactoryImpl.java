685bff5764711e5523ebbcf7bbb22c8365d135f0
Matt Champion <mattunderscorechampion@gmail.com>
2016-07-27 14:32:46 +0100

    Add ServiceSessionFactory.

---
+++ polling/src/main/java/com/pushtechnology/adapters/rest/polling/ServiceSessionFactoryImpl.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.polling;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+
+/**
+ * Implementation of {@link ServiceSessionFactory}.
+ *
+ * @author Push Technology Limited
+ */
+public final class ServiceSessionFactoryImpl implements ServiceSessionFactory {
+    private final ScheduledExecutorService executor;
+    private final EndpointClient endpointClient;
+    private final PollHandlerFactory handlerFactory;
+
+    /**
+     * Constructor.
+     */
+    public ServiceSessionFactoryImpl(
+            ScheduledExecutorService executor,
+            EndpointClient endpointClient,
+            PollHandlerFactory handlerFactory) {
+        this.executor = executor;
+        this.endpointClient = endpointClient;
+        this.handlerFactory = handlerFactory;
+    }
+
+    @Override
+    public ServiceSession create(ServiceConfig serviceConfig) {
+        return new ServiceSessionImpl(executor, endpointClient, serviceConfig, handlerFactory);
+    }
+}
---
+++ polling/src/test/java/com/pushtechnology/adapters/rest/polling/ServiceSessionFactoryImplTest.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.polling;
+
+import static java.util.Collections.singletonList;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.http.concurrent.FutureCallback;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+import com.pushtechnology.diffusion.datatype.json.JSON;
+
+/**
+ * Unit tests for {@link ServiceSessionFactoryImpl}.
+ *
+ * @author Push Technology Limited
+ */
+public final class ServiceSessionFactoryImplTest {
+    @Mock
+    private ScheduledExecutorService executor;
+    @Mock
+    private EndpointClient endpointClient;
+    @Mock
+    private PollHandlerFactory handlerFactory;
+    @Mock
+    private ScheduledFuture taskFuture;
+    @Mock
+    private FutureCallback<JSON> handler;
+
+    private final EndpointConfig endpointConfig = EndpointConfig
+        .builder()
+        .url("/a/url")
+        .build();
+    private final ServiceConfig serviceConfig = ServiceConfig
+        .builder()
+        .host("localhost")
+        .port(80)
+        .pollPeriod(5000L)
+        .endpoints(singletonList(endpointConfig))
+        .build();
+
+    private ServiceSessionFactory serviceSessionFactory;
+
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        when(executor
+            .scheduleWithFixedDelay(isA(Runnable.class), isA(Long.class), isA(Long.class), isA(TimeUnit.class)))
+            .thenReturn(taskFuture);
+        when(handlerFactory.create(serviceConfig, endpointConfig)).thenReturn(handler);
+
+        serviceSessionFactory = new ServiceSessionFactoryImpl(executor, endpointClient, handlerFactory);
+    }
+
+    @After
+    public void postConditions() {
+        verifyNoMoreInteractions(executor, endpointClient, handlerFactory, taskFuture);
+    }
+
+    @Test
+    public void startSuccessfulPoll() {
+        final ServiceSession serviceSession = serviceSessionFactory.create(serviceConfig);
+
+        assertTrue(serviceSession instanceof ServiceSessionImpl);
+
+        serviceSession.start();
+        serviceSession.addEndpoint(endpointConfig);
+        verify(handlerFactory).create(serviceConfig, endpointConfig);
+
+        verify(executor).scheduleWithFixedDelay(isA(Runnable.class), eq(0L), eq(5000L), eq(MILLISECONDS));
+    }
+}
