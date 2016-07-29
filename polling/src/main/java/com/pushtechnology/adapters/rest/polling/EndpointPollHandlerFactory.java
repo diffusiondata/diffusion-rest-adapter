3fdca9cea47b28d87816bb188fa27758730ae920
Matt Champion <mattunderscorechampion@gmail.com>
2016-07-29 12:02:06 +0100

    Add a PollHandlerFactory that returns a handler suitable for the endpoint.

---
+++ polling/src/main/java/com/pushtechnology/adapters/rest/polling/EndpointPollHandlerFactory.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.polling;
+
+import org.apache.http.concurrent.FutureCallback;
+
+import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+import com.pushtechnology.diffusion.datatype.binary.Binary;
+import com.pushtechnology.diffusion.datatype.json.JSON;
+
+/**
+ * Factory for poll handlers of the type returned by the endpoint.
+ * @author Push Technology Limited
+ */
+public final class EndpointPollHandlerFactory implements PollHandlerFactory<String> {
+    private final PollHandlerFactory<JSON> jsonHandlerFactory;
+    private final PollHandlerFactory<Binary> binaryHandlerFactory;
+    private final PollHandlerFactory<String> stringHandlerFactory;
+
+    /**
+     * Constructor.
+     */
+    public EndpointPollHandlerFactory(
+            PollHandlerFactory<JSON> jsonHandlerFactory,
+            PollHandlerFactory<Binary> binaryHandlerFactory,
+            PollHandlerFactory<String> stringHandlerFactory) {
+        this.jsonHandlerFactory = jsonHandlerFactory;
+        this.binaryHandlerFactory = binaryHandlerFactory;
+        this.stringHandlerFactory = stringHandlerFactory;
+    }
+
+    @Override
+    public FutureCallback<String> create(ServiceConfig serviceConfig, EndpointConfig endpointConfig) {
+        final String produces = endpointConfig.getProduces();
+        switch (produces) {
+            case "json" :
+                return new JSONParsingHandler(jsonHandlerFactory.create(serviceConfig, endpointConfig));
+            case "binary" :
+                return new BinaryParsingHandler(binaryHandlerFactory.create(serviceConfig, endpointConfig));
+            case "text/plain" :
+                return stringHandlerFactory.create(serviceConfig, endpointConfig);
+            default:
+                throw new IllegalArgumentException("Unsupported produces value \"" + produces + "\"");
+        }
+    }
+}
---
+++ polling/src/test/java/com/pushtechnology/adapters/rest/polling/EndpointPollHandlerFactoryTest.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.polling;
+
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+import com.pushtechnology.diffusion.datatype.binary.Binary;
+import com.pushtechnology.diffusion.datatype.json.JSON;
+
+/**
+ * Unit tests for {@link EndpointPollHandlerFactory}.
+ *
+ * @author Push Technology Limited
+ */
+public final class EndpointPollHandlerFactoryTest {
+    @Mock
+    private PollHandlerFactory<JSON> jsonPollHandlerFactory;
+    @Mock
+    private PollHandlerFactory<Binary> binaryPollHandlerFactory;
+    @Mock
+    private PollHandlerFactory<String> stringPollHandlerFactory;
+
+    private final EndpointConfig jsonEndpoint = EndpointConfig
+        .builder()
+        .url("/a/url/json")
+        .produces("json")
+        .build();
+    private final EndpointConfig binaryEndpoint = EndpointConfig
+        .builder()
+        .url("/a/url/binary")
+        .produces("binary")
+        .build();
+    private final EndpointConfig plainTextEndpoint = EndpointConfig
+        .builder()
+        .url("/a/url/text")
+        .produces("text/plain")
+        .build();
+    private final EndpointConfig xmlEndpoint = EndpointConfig
+        .builder()
+        .url("/a/url/text")
+        .produces("text/xml")
+        .build();
+    private final ServiceConfig serviceConfig = ServiceConfig
+        .builder()
+        .host("localhost")
+        .port(80)
+        .pollPeriod(5000L)
+        .endpoints(asList(jsonEndpoint, binaryEndpoint, plainTextEndpoint, xmlEndpoint))
+        .build();
+
+    private PollHandlerFactory<String> pollHandlerFactory;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        pollHandlerFactory = new EndpointPollHandlerFactory(
+            jsonPollHandlerFactory,
+            binaryPollHandlerFactory,
+            stringPollHandlerFactory);
+    }
+
+    @After
+    public void postConditions() {
+        verifyNoMoreInteractions(jsonPollHandlerFactory, binaryPollHandlerFactory, stringPollHandlerFactory);
+    }
+
+    @Test
+    public void createJson() {
+        pollHandlerFactory.create(serviceConfig, jsonEndpoint);
+
+        verify(jsonPollHandlerFactory).create(serviceConfig, jsonEndpoint);
+    }
+
+    @Test
+    public void createBinary() {
+        pollHandlerFactory.create(serviceConfig, binaryEndpoint);
+
+        verify(binaryPollHandlerFactory).create(serviceConfig, binaryEndpoint);
+    }
+
+    @Test
+    public void createPlainText() {
+        pollHandlerFactory.create(serviceConfig, plainTextEndpoint);
+
+        verify(stringPollHandlerFactory).create(serviceConfig, plainTextEndpoint);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createXML() {
+        pollHandlerFactory.create(serviceConfig, xmlEndpoint);
+    }
+}
