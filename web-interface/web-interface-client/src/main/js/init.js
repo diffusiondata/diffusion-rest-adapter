9ef298b76653bd0d6616bf2417becd42f7e19009
Matt Champion <mattunderscorechampion@gmail.com>
2016-09-05 14:09:50 +0100

    Simplify application bootstrap.

--- web-interface/web-interface-client/src/main/js/init.js
+++ web-interface/web-interface-client/src/main/js/init.js
@@ -1,72 +1,9 @@
 
 (function() {
-    var diffusion = require('diffusion');
-    var model = require('model');
+    // TODO: Bootstrap TypScript without JavaScript
+    // Pull in Angular 2 dependencies
     var reflect = require('reflect-metadata');
     var zone = require('zone.js');
-    var view;
-
-    function start() {
-        console.log('Ready');
-
-        var main = require('main');
-
-        session
-            .stream('?adapter/rest/model/store/')
-            .asType(diffusion.datatypes.json())
-            .on('value', function(path, specification, newValue, oldValue) {
-                console.log(path, newValue);
-            });
-    }
-
-    var started = false;
-    function tryStart() {
-        if (!started && setup && connected) {
-            start();
-
-            started = true;
-        }
-    }
-
-    var connected = false;
-    var session;
-    function connect() {
-        diffusion.connect({
-            host : 'localhost',
-            port : 8080
-        }).then(function (newSession) {
-            connected = true;
-            session = newSession;
-            session.subscribe('?adapter/rest/model/store/');
-
-            tryStart();
-        }, function() {
-            setTimeout(connect, 5000);
-        });
-    }
-
-    var setup = false;
-    function ready() {
-        if (setup) {
-            return;
-        }
-
-        tryStart();
-
-        setup = true;
-    }
-
-    if (document.readyState === 'complete') {
-        setTimeout(ready, 0);
-    }
-    else if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', ready, false);
-        window.addEventListener('load', ready, false);
-    }
-    else {
-        document.attachEvent('onreadystatechange', readyStateChange);
-        window.attachEvent('onload', ready);
-    }
-
-    connect();
+    // Pull in Angular 2 application
+    var main = require('main');
 })();
