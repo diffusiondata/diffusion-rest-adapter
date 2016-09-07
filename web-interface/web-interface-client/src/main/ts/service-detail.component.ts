74879366cc1a8762e464ff7007056c4f276a5c80
Matt Champion <mattunderscorechampion@gmail.com>
2016-09-07 17:38:44 +0100

    Improve detail styling.

--- web-interface/web-interface-client/src/main/ts/service-detail.component.ts
+++ web-interface/web-interface-client/src/main/ts/service-detail.component.ts
@@ -6,14 +6,29 @@ import { ModelService } from './model.service';
 
 @Component({
   selector: 'service-detail',
-  template: `<div *ngIf="service">
+  template: `<form *ngIf="service" class="form-horizontal">
     <h3>{{service.name}}</h3>
-    <div><span>Host</span><span>{{service.host}}</span></div>
-    <div><span>Port</span><span>{{service.port}}</span></div>
-    <div><span>Secure</span><span>{{service.secure}}</span></div>
-    <div><span>Poll period</span><span>{{service.pollPeriod}}</span></div>
-    <div><span>Topic root</span><span>{{service.topicRoot}}</span></div>
-</div>`
+    <div class="form-group">
+        <label for="host" class="col-sm-2 control-label">Host</label>
+        <p id="host" class="form-control-static">{{service.host}}</p>
+    </div>
+    <div class="form-group">
+        <label for="port" class="col-sm-2 control-label">Port</label>
+        <p id="port" class="form-control-static">{{service.port}}</p>
+    </div>
+    <div class="form-group">
+        <label for="secure" class="col-sm-2 control-label">Secure</label>
+        <p id="secure" class="form-control-static">{{service.secure}}</p>
+    </div>
+    <div class="form-group">
+        <label for="pollPeriod" class="col-sm-2 control-label">Poll period</label>
+        <p id="pollPeriod" class="form-control-static">{{service.pollPeriod}}</p>
+    </div>
+    <div class="form-group">
+        <label for="topicRoot" class="col-sm-2 control-label">Topic root</label>
+        <p id="topicRoot" class="form-control-static">{{service.topicRoot}}</p>
+    </div>
+</form>`
 })
 export class ServiceDetailComponent implements OnInit {
     private service: Service;
