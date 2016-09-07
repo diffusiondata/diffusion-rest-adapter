ef35279d693d84ce0df651e669d34a912d4e94f8
Matt Champion <mattunderscorechampion@gmail.com>
2016-09-07 17:38:55 +0100

    Improve create styling.

--- web-interface/web-interface-client/src/main/ts/create-service.component.ts
+++ web-interface/web-interface-client/src/main/ts/create-service.component.ts
@@ -7,52 +7,64 @@ import { Service } from './model';
 @Component({
   selector: 'create-service',
   template: `<h3>Create service</h3>
-<form *ngIf="active" #createServiceForm="ngForm" (ngSubmit)="onCreateService($event)">
+<form *ngIf="active" #createServiceForm="ngForm" (ngSubmit)="onCreateService($event)" class="form-horizontal">
     <div class="form-group">
-        <label for="name">Name</label>
-        <input id="name" required [(ngModel)]="service.name" name="name" #name="ngModel">
-        <div [hidden]="name.valid || name.pristine"
-             class="alert alert-danger">
-          Name is required
+        <label for="name" class="col-sm-2 control-label">Name</label>
+        <div class="col-sm-10">
+            <input id="name" required [(ngModel)]="service.name" name="name" #name="ngModel">
+            <div [hidden]="name.valid || name.pristine"
+                 class="alert alert-danger">
+              Name is required
+            </div>
         </div>
     </div>
     <div class="form-group">
-        <label for="host">Host</label>
+        <label for="host" class="col-sm-2 control-label">Host</label>
+        <div class="col-sm-10">
         <input id="host" required [(ngModel)]="service.host" name="host" #host="ngModel">
         <div [hidden]="host.valid || host.pristine"
              class="alert alert-danger">
           Host is required
         </div>
-    </div>
-    <div class="form-group">
-        <label for="port">Port</label>
-        <input id="port" required [(ngModel)]="service.port" name="port" #port="ngModel">
-        <div [hidden]="port.valid || port.pristine"
-             class="alert alert-danger">
-          Port is required
         </div>
     </div>
     <div class="form-group">
-        <label for="secure">Secure</label>
-        <select id="secure" required [(ngModel)]="service.secure" name="secure">
-            <option value="true">Secure</option>
-            <option value="false">Insecure</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="pollPeriod">Poll period</label>
-        <input id="pollPeriod" required [(ngModel)]="service.pollPeriod" name="pollPeriod" #pollPeriod="ngModel">
-        <div [hidden]="pollPeriod.valid || pollPeriod.pristine"
-             class="alert alert-danger">
-          Poll period is required
+        <label for="port" class="col-sm-2 control-label">Port</label>
+        <div class="col-sm-10">
+            <input id="port" required [(ngModel)]="service.port" name="port" #port="ngModel">
+            <div [hidden]="port.valid || port.pristine"
+                 class="alert alert-danger">
+              Port is required
+            </div>
         </div>
     </div>
     <div class="form-group">
-        <label for="topicRoot">Topic root</label>
-        <input id="topicRoot" required [(ngModel)]="service.topicRoot" name="topicRoot" #topicRoot="ngModel">
-        <div [hidden]="name.valid || name.pristine"
-             class="alert alert-danger">
-          Topic root is required
+        <label for="secure" class="col-sm-2 control-label">Secure</label>
+        <div class="col-sm-10">
+            <select id="secure" required [(ngModel)]="service.secure" name="secure">
+                <option value="true">Secure</option>
+                <option value="false">Insecure</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="pollPeriod" class="col-sm-2 control-label">Poll period</label>
+        <div class="col-sm-10">
+            <input id="pollPeriod" required [(ngModel)]="service.pollPeriod" name="pollPeriod" #pollPeriod="ngModel">
+            <div [hidden]="pollPeriod.valid || pollPeriod.pristine"
+                 class="alert alert-danger">
+              Poll period is required
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="topicRoot" class="col-sm-2 control-label">Topic root</label>
+        <div class="col-sm-10">
+            <input id="topicRoot" required [(ngModel)]="service.topicRoot" name="topicRoot" #topicRoot="ngModel">
+            <div [hidden]="name.valid || name.pristine"
+                 class="alert alert-danger">
+              Topic root is required
+            </div>
         </div>
     </div>
     <div>
