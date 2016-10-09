1a0fc19c449e784173949827a492075296b3e64d
Matt Champion <mattunderscorechampion@gmail.com>
2016-10-09 22:47:09 +0100

    Add client side unit tests for adding and deleting services and endpoints.

--- web-interface/web-interface-client/src/test/js/model.service-spec.js
+++ web-interface/web-interface-client/src/test/js/model.service-spec.js
@@ -11,8 +11,6 @@ describe('Model service', function() {
     var diffusionService;
     var session;
     var onMessage;
-    var onSendSuccess;
-    var onSendFailure;
     var modelService;
 
     beforeEach(function() {
@@ -24,9 +22,14 @@ describe('Model service', function() {
             messages : jasmine.createSpyObj('messages', ['listen', 'send'])
         };
         onMessage = when.captor();
-        onSendSuccess = when.captor();
-        onSendFailure = when.captor();
 
+        when(session.messages.listen).isCalledWith('adapter/rest/model/store', onMessage);
+        when(diffusionService.get).isCalled.thenReturn(Promise.resolve(session));
+
+        modelService = new ms.ModelService(diffusionService);
+    });
+
+    function respondWithSuccesfulModelLookup() {
         when(session.messages.send).isCalled.then(function() {
             onMessage.latest({
                 content: jsonDataType.writeValue({
@@ -37,18 +40,26 @@ describe('Model service', function() {
                 })
             });
         });
-        when(session.messages.listen).isCalledWith('adapter/rest/model/store', onMessage);
+    }
 
-        when(diffusionService.get).isCalled.thenReturn(Promise.resolve(session));
-
-        modelService = new ms.ModelService(diffusionService);
-    });
+    function respondWithSuccess() {
+        when(session.messages.send).isCalled.then(function() {
+            onMessage.latest({
+                content: jsonDataType.writeValue({
+                    id: 0,
+                    response: {}
+                })
+            });
+        });
+    }
 
     it('can be created', function() {
         expect(modelService).toBeDefined();
     });
 
     it('can lookup model', function(done) {
+        respondWithSuccesfulModelLookup();
+
         var modelPromise = modelService.getModel();
         expect(diffusionService.get).toHaveBeenCalled();
         expect(modelPromise).toBeDefined();
@@ -60,14 +71,90 @@ describe('Model service', function() {
         }, done.fail);
     });
 
-    it('can lookup service', function(done) {
-        var modelPromise = modelService.getService('service0');
-        expect(diffusionService.get).toHaveBeenCalled();
-        expect(modelPromise).toBeDefined();
+    it('can lookup a service', function(done) {
+        respondWithSuccesfulModelLookup();
 
-        modelPromise.then(function(service) {
+        var servicePromise = modelService.getService('service0');
+        expect(diffusionService.get).toHaveBeenCalled();
+        expect(servicePromise).toBeDefined();
+
+        servicePromise.then(function(service) {
             expect(session.messages.send).toHaveBeenCalledWith('adapter/rest/model/store', jasmine.anything());
             expect(service.name).toBe('service0');
+            done();
+        }, done.fail);
+    });
+
+    it('can create a service', function(done) {
+        respondWithSuccess();
+
+        var createPromise = modelService.createService({
+            name: 'service0'
+        });
+        expect(diffusionService.get).toHaveBeenCalled();
+        expect(createPromise).toBeDefined();
+
+        createPromise.then(function() {
+            expect(session.messages.send).toHaveBeenCalledWith('adapter/rest/model/store', jasmine.anything());
+            expect(modelService.model.services[0].name).toBe('service0');
+            done();
+        }, done.fail);
+    });
+
+    it('can create an endpoint', function(done) {
+        respondWithSuccess();
+        modelService.model.services[0] = {
+            name: 'service0',
+            endpoints: []
+        };
+
+        var createPromise = modelService.createEndpoint('service0', {
+            name: 'endpoint0'
+        });
+        expect(diffusionService.get).toHaveBeenCalled();
+        expect(createPromise).toBeDefined();
+
+        createPromise.then(function() {
+            expect(session.messages.send).toHaveBeenCalledWith('adapter/rest/model/store', jasmine.anything());
+            expect(modelService.model.services[0].name).toBe('service0');
+            done();
+        }, done.fail);
+    });
+
+    it('can delete a service', function(done) {
+        respondWithSuccess();
+        modelService.model.services[0] = {
+            name: 'service0',
+            endpoints: []
+        };
+
+        var deletePromise = modelService.deleteService('service0');
+        expect(diffusionService.get).toHaveBeenCalled();
+        expect(deletePromise).toBeDefined();
+
+        deletePromise.then(function() {
+            expect(session.messages.send).toHaveBeenCalledWith('adapter/rest/model/store', jasmine.anything());
+            expect(modelService.model.services.length).toBe(0);
+            done();
+        }, done.fail);
+    });
+
+    it('can delete an deleteEndpoint', function(done) {
+        respondWithSuccess();
+        modelService.model.services[0] = {
+            name: 'service0',
+            endpoints: [{
+                name: 'endpoint0'
+            }]
+        };
+
+        var deletePromise = modelService.deleteEndpoint('service0', 'endpoint0');
+        expect(diffusionService.get).toHaveBeenCalled();
+        expect(deletePromise).toBeDefined();
+
+        deletePromise.then(function() {
+            expect(session.messages.send).toHaveBeenCalledWith('adapter/rest/model/store', jasmine.anything());
+            expect(modelService.model.services[0].endpoints.length).toBe(0);
             done();
         }, done.fail);
     });
