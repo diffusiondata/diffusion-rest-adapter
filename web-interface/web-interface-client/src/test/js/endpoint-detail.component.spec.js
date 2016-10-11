207ee3ad8442cd4001fd85cf7a1ecd35cdcca674
Matt Champion <mattunderscorechampion@gmail.com>
2016-10-11 22:29:00 +0100

    Add unit test for the endpoint detail component.

---
+++ web-interface/web-interface-client/src/test/js/endpoint-detail.component.spec.js
@@ -0,0 +1,33 @@
+
+require('reflect-metadata');
+
+var edc = require('../../../target/js/endpoint-detail.component'),
+    when = require('saywhen');
+
+describe('Endpoint detail component', function() {
+    var modelService;
+    var component;
+
+    beforeEach(function() {
+        modelService = jasmine.createSpyObj('modelService', ['deleteEndpoint']);
+
+        component = new edc.EndpointDetailComponent(modelService);
+        // Injected by Angular
+        component.service = {
+            name: 'service0'
+        };
+        component.endpoint = {
+            name: 'endpoint0'
+        };
+    });
+
+    it('can be created', function() {
+        expect(component).toBeDefined();
+    });
+
+    it('can remove the endpoint', function() {
+        component.onRemove('service0', 'endpoint0');
+
+        expect(modelService.deleteEndpoint).toHaveBeenCalledWith('service0', 'endpoint0');
+    });
+});
