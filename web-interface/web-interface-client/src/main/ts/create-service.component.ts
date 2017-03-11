2838971aaa636610564ddee22bf43dbe62decbfd
Matt Champion <mattunderscorechampion@gmail.com>
2017-03-11 16:54:00 +0000

    Remove unused row div elements.

--- web-interface/web-interface-client/src/main/ts/create-service.component.ts
+++ web-interface/web-interface-client/src/main/ts/create-service.component.ts
@@ -7,87 +7,85 @@ import { Service } from './model';
 @Component({
   selector: 'create-service',
   template: `<services-list></services-list>
-<div class="row">
-    <div class="col-md-8">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Create service</h3>
-            </div>
-            <div class="panel-body">
-                <form *ngIf="active" #createServiceForm="ngForm" (ngSubmit)="onCreateService($event)" class="form-horizontal">
-                    <div class="form-group" [class.has-error]="!name.valid && !name.pristine">
-                        <label for="name" class="col-sm-3 control-label">Name</label>
-                        <div class="col-sm-4">
-                            <input id="name" required [(ngModel)]="service.name" name="name" #name="ngModel" class="form-control">
-                        </div>
-                        <span class="help-block col-sm-4">The name is required to uniquely identify the service</span>
+<div class="col-md-8">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Create service</h3>
+        </div>
+        <div class="panel-body">
+            <form *ngIf="active" #createServiceForm="ngForm" (ngSubmit)="onCreateService($event)" class="form-horizontal">
+                <div class="form-group" [class.has-error]="!name.valid && !name.pristine">
+                    <label for="name" class="col-sm-3 control-label">Name</label>
+                    <div class="col-sm-4">
+                        <input id="name" required [(ngModel)]="service.name" name="name" #name="ngModel" class="form-control">
                     </div>
-                    <div class="form-group" [class.has-error]="!host.valid && !host.pristine">
-                        <label for="host" class="col-sm-3 control-label">Host</label>
-                        <div class="col-sm-4">
-                            <input id="host" required [(ngModel)]="service.host" name="host" #host="ngModel" class="form-control">
-                        </div>
-                        <span class="help-block col-sm-4">The host is required to to describe the location of the service</span>
+                    <span class="help-block col-sm-4">The name is required to uniquely identify the service</span>
+                </div>
+                <div class="form-group" [class.has-error]="!host.valid && !host.pristine">
+                    <label for="host" class="col-sm-3 control-label">Host</label>
+                    <div class="col-sm-4">
+                        <input id="host" required [(ngModel)]="service.host" name="host" #host="ngModel" class="form-control">
                     </div>
-                    <div class="form-group" [class.has-error]="!port.valid && !port.pristine">
-                        <label for="port" class="col-sm-3 control-label">Port</label>
-                        <div class="col-sm-4">
-                            <input id="port" required [(ngModel)]="service.port" name="port" #port="ngModel" class="form-control">
-                        </div>
-                        <span class="help-block col-sm-4">The port is required to to describe the location of the service</span>
+                    <span class="help-block col-sm-4">The host is required to to describe the location of the service</span>
+                </div>
+                <div class="form-group" [class.has-error]="!port.valid && !port.pristine">
+                    <label for="port" class="col-sm-3 control-label">Port</label>
+                    <div class="col-sm-4">
+                        <input id="port" required [(ngModel)]="service.port" name="port" #port="ngModel" class="form-control">
                     </div>
-                    <div class="form-group">
-                        <label for="secure" class="col-sm-3 control-label">Secure</label>
-                        <div class="col-sm-4">
-                            <select id="secure" required [(ngModel)]="service.secure" name="secure" class="form-control">
-                                <option value="true">Secure</option>
-                                <option value="false">Insecure</option>
-                            </select>
-                        </div>
-                        <span class="help-block col-sm-4">Indicates how to communicate with the service</span>
+                    <span class="help-block col-sm-4">The port is required to to describe the location of the service</span>
+                </div>
+                <div class="form-group">
+                    <label for="secure" class="col-sm-3 control-label">Secure</label>
+                    <div class="col-sm-4">
+                        <select id="secure" required [(ngModel)]="service.secure" name="secure" class="form-control">
+                            <option value="true">Secure</option>
+                            <option value="false">Insecure</option>
+                        </select>
                     </div>
-                    <div class="form-group" [class.has-error]="!pollPeriod.valid && !pollPeriod.pristine">
-                        <label for="pollPeriod" class="col-sm-3 control-label">Poll period</label>
-                        <div class="col-sm-4">
-                            <input id="pollPeriod" required [(ngModel)]="service.pollPeriod" name="pollPeriod" #pollPeriod="ngModel" class="form-control">
-                        </div>
-                        <span class="help-block col-sm-4">Indicates how frequently the service endpoints should be polled</span>
+                    <span class="help-block col-sm-4">Indicates how to communicate with the service</span>
+                </div>
+                <div class="form-group" [class.has-error]="!pollPeriod.valid && !pollPeriod.pristine">
+                    <label for="pollPeriod" class="col-sm-3 control-label">Poll period</label>
+                    <div class="col-sm-4">
+                        <input id="pollPeriod" required [(ngModel)]="service.pollPeriod" name="pollPeriod" #pollPeriod="ngModel" class="form-control">
                     </div>
-                    <div class="form-group" [class.has-error]="!topicPathRoot.valid && !topicPathRoot.pristine">
-                        <label for="topicPathRoot" class="col-sm-3 control-label">Topic path root</label>
-                        <div class="col-sm-4">
-                            <input id="topicPathRoot" required [(ngModel)]="service.topicPathRoot" name="topicPathRoot" #topicPathRoot="ngModel" class="form-control">
-                        </div>
-                        <span class="help-block col-sm-4">Indicates the Diffusion topic that the service is published under</span>
+                    <span class="help-block col-sm-4">Indicates how frequently the service endpoints should be polled</span>
+                </div>
+                <div class="form-group" [class.has-error]="!topicPathRoot.valid && !topicPathRoot.pristine">
+                    <label for="topicPathRoot" class="col-sm-3 control-label">Topic path root</label>
+                    <div class="col-sm-4">
+                        <input id="topicPathRoot" required [(ngModel)]="service.topicPathRoot" name="topicPathRoot" #topicPathRoot="ngModel" class="form-control">
                     </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <a role="button" href="#basicAuthPanel" data-toggle="collapse"><h3 class="panel-title">Basic authentication<span class="pull-right clickable fa fa-chevron-down"></span></h3></a>
-                        </div>
-                        <div class="panel-body collapse" id="basicAuthPanel">
-                            <div class="form-group">
-                                <label for="userid" class="col-sm-3 control-label">User ID</label>
-                                <div class="col-sm-4">
-                                    <input id="userid" [(ngModel)]="authentication.userid" name="userid" #userid="ngModel" class="form-control">
-                                </div>
-                                <span class="help-block col-sm-4">The user ID for basic authentication</span>
+                    <span class="help-block col-sm-4">Indicates the Diffusion topic that the service is published under</span>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a role="button" href="#basicAuthPanel" data-toggle="collapse"><h3 class="panel-title">Basic authentication<span class="pull-right clickable fa fa-chevron-down"></span></h3></a>
+                    </div>
+                    <div class="panel-body collapse" id="basicAuthPanel">
+                        <div class="form-group">
+                            <label for="userid" class="col-sm-3 control-label">User ID</label>
+                            <div class="col-sm-4">
+                                <input id="userid" [(ngModel)]="authentication.userid" name="userid" #userid="ngModel" class="form-control">
                             </div>
-                            <div class="form-group">
-                                <label for="password" class="col-sm-3 control-label">Password</label>
-                                <div class="col-sm-4">
-                                    <input id="password" [(ngModel)]="authentication.password" name="password" #password="ngModel" class="form-control" type="password">
-                                </div>
-                                <span class="help-block col-sm-4">The password for basic authentication</span>
+                            <span class="help-block col-sm-4">The user ID for basic authentication</span>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-sm-3 control-label">Password</label>
+                            <div class="col-sm-4">
+                                <input id="password" [(ngModel)]="authentication.password" name="password" #password="ngModel" class="form-control" type="password">
                             </div>
+                            <span class="help-block col-sm-4">The password for basic authentication</span>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="col-sm-offset-3 col-sm-8">
-                            <button class="btn btn-default" [disabled]="!createServiceForm.form.valid" type="submit">Create service</button>
-                        </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-3 col-sm-8">
+                        <button class="btn btn-default" [disabled]="!createServiceForm.form.valid" type="submit">Create service</button>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>`
--- web-interface/web-interface-client/src/main/ts/service-detail.component.ts
+++ web-interface/web-interface-client/src/main/ts/service-detail.component.ts
@@ -7,48 +7,46 @@ import { ModelService } from './model.service';
 @Component({
   selector: 'service-detail',
   template: `<services-list></services-list>
-<div class="row">
-    <div class="col-md-8">
-        <div *ngIf="service" class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">{{service.name}}</h3>
-            </div>
-            <div class="panel-body">
-                <div class="form-horizontal">
-                    <div class="form-group">
-                        <label for="host" class="col-sm-2 control-label">Host</label>
-                        <p id="host" class="form-control-static col-sm-4">{{service.host}}</p>
-                    </div>
-                    <div class="form-group">
-                        <label for="port" class="col-sm-2 control-label">Port</label>
-                        <p id="port" class="form-control-static col-sm-4">{{service.port}}</p>
-                    </div>
-                    <div class="form-group">
-                        <label for="secure" class="col-sm-2 control-label">Secure</label>
-                        <p id="secure" class="form-control-static col-sm-4">{{service.secure}}</p>
-                    </div>
-                    <div class="form-group">
-                        <label for="pollPeriod" class="col-sm-2 control-label">Poll period</label>
-                        <p id="pollPeriod" class="form-control-static col-sm-4">{{service.pollPeriod}}</p>
-                    </div>
-                    <div class="form-group">
-                        <label for="topicPathRoot" class="col-sm-2 control-label">Topic path root</label>
-                        <p id="topicPathRoot" class="form-control-static col-sm-4">{{service.topicPathRoot}}</p>
-                    </div>
-                    <div class="form-group" *ngIf="service.security && service.security.basic">
-                        <label for="basicUserID" class="col-sm-2 control-label">User ID</label>
-                        <p id="basicUserID" class="form-control-static col-sm-4">{{service.security.basic.userid}}</p>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-8">
-                            <button class="btn btn-default" (click)="onRemove()">Remove service</button>
-                        </div>
+<div class="col-md-8">
+    <div *ngIf="service" class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">{{service.name}}</h3>
+        </div>
+        <div class="panel-body">
+            <div class="form-horizontal">
+                <div class="form-group">
+                    <label for="host" class="col-sm-2 control-label">Host</label>
+                    <p id="host" class="form-control-static col-sm-4">{{service.host}}</p>
+                </div>
+                <div class="form-group">
+                    <label for="port" class="col-sm-2 control-label">Port</label>
+                    <p id="port" class="form-control-static col-sm-4">{{service.port}}</p>
+                </div>
+                <div class="form-group">
+                    <label for="secure" class="col-sm-2 control-label">Secure</label>
+                    <p id="secure" class="form-control-static col-sm-4">{{service.secure}}</p>
+                </div>
+                <div class="form-group">
+                    <label for="pollPeriod" class="col-sm-2 control-label">Poll period</label>
+                    <p id="pollPeriod" class="form-control-static col-sm-4">{{service.pollPeriod}}</p>
+                </div>
+                <div class="form-group">
+                    <label for="topicPathRoot" class="col-sm-2 control-label">Topic path root</label>
+                    <p id="topicPathRoot" class="form-control-static col-sm-4">{{service.topicPathRoot}}</p>
+                </div>
+                <div class="form-group" *ngIf="service.security && service.security.basic">
+                    <label for="basicUserID" class="col-sm-2 control-label">User ID</label>
+                    <p id="basicUserID" class="form-control-static col-sm-4">{{service.security.basic.userid}}</p>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-8">
+                        <button class="btn btn-default" (click)="onRemove()">Remove service</button>
                     </div>
                 </div>
             </div>
         </div>
-        <endpoints-list></endpoints-list>
     </div>
+    <endpoints-list></endpoints-list>
 </div>`
 })
 export class ServiceDetailComponent implements OnInit {
