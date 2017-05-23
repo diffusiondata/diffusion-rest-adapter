d48a2936e05fdf826c6b6e6e95b2991e7a2c5043
Matt Champion <mattunderscorechampion@gmail.com>
2017-05-23 11:43:47 +0100

    Add interfaces for metrics events.

--- metrics/metrics-events/src/main/kotlin/events.kt
+++ metrics/metrics-events/src/main/kotlin/events.kt
@@ -11,23 +11,55 @@ import java.lang.Exception
  *
  * @author Matt Champion 17/05/2017
  */
-data class TopicCreationRequestEvent(
+interface ITopicCreationRequestEvent {
     /**
      * @return the topic path
      */
-    val path: String,
+    val path: String
     /**
      * @return the topic type
      */
-    val topicType: TopicType,
+    val topicType: TopicType
     /**
      * @return the length of any initial value
      */
-    val initialValueLength: Int,
+    val initialValueLength: Int
     /**
      * @return the request timestamp
      */
-    val requestTimestamp: Long)
+    val requestTimestamp: Long
+}
+
+/**
+ * Implementation of {@link ITopicCreationRequestEvent}.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+data class TopicCreationRequestEvent(
+    override val path: String,
+    override val topicType: TopicType,
+    override val initialValueLength: Int,
+    override val requestTimestamp: Long): ITopicCreationRequestEvent
+
+/**
+ * Event describing a successful topic creation.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+interface ITopicCreationSuccessEvent {
+    /**
+     * @return the topic creation request event
+     */
+    val requestEvent: ITopicCreationRequestEvent
+    /**
+     * @return the success timestamp
+     */
+    val successTimestamp: Long
+    /**
+     * @return the request timestamp
+     */
+    val requestTime: Long
+}
 
 /**
  * Event describing a successful topic creation.
@@ -35,19 +67,10 @@ data class TopicCreationRequestEvent(
  * @author Matt Champion 17/05/2017
  */
 data class TopicCreationSuccessEvent(
-    /**
-     * @return the topic creation request event
-     */
-    val requestEvent: TopicCreationRequestEvent,
-    /**
-     * @return the success timestamp
-     */
-    val successTimestamp: Long) {
+    override val requestEvent: ITopicCreationRequestEvent,
+    override val successTimestamp: Long) : ITopicCreationSuccessEvent {
 
-    /**
-     * @return the request timestamp
-     */
-    val requestTime: Long
+    override val requestTime: Long
         get() = successTimestamp - requestEvent.requestTimestamp
 }
 
@@ -56,24 +79,36 @@ data class TopicCreationSuccessEvent(
  *
  * @author Matt Champion 17/05/2017
  */
-data class TopicCreationFailedEvent(
+interface ITopicCreationFailedEvent {
     /**
      * @return the topic creation request event
      */
-    val requestEvent: TopicCreationRequestEvent,
+    val requestEvent: ITopicCreationRequestEvent
     /**
      * @return the failure reason
      */
-    val failReason: TopicAddFailReason,
+    val failReason: TopicAddFailReason
     /**
      * @return the failure timestamp
      */
-    val failedTimestamp: Long) {
-
+    val failedTimestamp: Long
     /**
      * @return the time to failure
      */
     val requestTime: Long
+}
+
+/**
+ * Event describing a failed topic creation.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+data class TopicCreationFailedEvent(
+    override val requestEvent: ITopicCreationRequestEvent,
+    override val failReason: TopicAddFailReason,
+    override val failedTimestamp: Long) : ITopicCreationFailedEvent {
+
+    override val requestTime: Long
         get() = failedTimestamp - requestEvent.requestTimestamp
 }
 
@@ -82,15 +117,53 @@ data class TopicCreationFailedEvent(
  *
  * @author Matt Champion 17/05/2017
  */
-data class PollRequestEvent(
+interface IPollRequestEvent {
     /**
      * @return the URI
      */
-    val uri: String,
+    val uri: String
     /**
      * @return the request timestamp
      */
-    val requestTimestamp: Long)
+    val requestTimestamp: Long
+}
+
+/**
+ * Event describing a poll request.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+data class PollRequestEvent(
+    override val uri: String,
+    override val requestTimestamp: Long) : IPollRequestEvent
+
+/**
+ * Event describing a successful poll request.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+interface IPollSuccessEvent {
+    /**
+     * @return the poll request event
+     */
+    val requestEvent: IPollRequestEvent
+    /**
+     * @return the status code of the response
+     */
+    val statusCode: Int
+    /**
+     * @return the length of the response
+     */
+    val responseLength: Long
+    /**
+     * @return the success timestamp
+     */
+    val successTimestamp: Long
+    /**
+     * @return the request timestamp
+     */
+    val requestTime: Long
+}
 
 /**
  * Event describing a successful poll request.
@@ -98,27 +171,12 @@ data class PollRequestEvent(
  * @author Matt Champion 17/05/2017
  */
 data class PollSuccessEvent(
-    /**
-     * @return the poll request event
-     */
-    val requestEvent: PollRequestEvent,
-    /**
-     * @return the status code of the response
-     */
-    val statusCode: Int,
-    /**
-     * @return the length of the response
-     */
-    val responseLength: Long,
-    /**
-     * @return the success timestamp
-     */
-    val successTimestamp: Long) {
+    override val requestEvent: IPollRequestEvent,
+    override val statusCode: Int,
+    override val responseLength: Long,
+    override val successTimestamp: Long) : IPollSuccessEvent {
 
-    /**
-     * @return the request timestamp
-     */
-    val requestTime: Long
+    override val requestTime: Long
         get() = successTimestamp - requestEvent.requestTimestamp
 }
 
@@ -127,20 +185,19 @@ data class PollSuccessEvent(
  *
  * @author Matt Champion 17/05/2017
  */
-data class PollFailedEvent(
+interface IPollFailedEvent {
     /**
      * @return the poll request event
      */
-    val requestEvent: PollRequestEvent,
+    val requestEvent: IPollRequestEvent
     /**
      * @return the exception
      */
-    val exception: Exception,
+    val exception: Exception
     /**
      * @return the failure timestamp
      */
-    val failedTimestamp: Long) {
-
+    val failedTimestamp: Long
     /**
      * @return the time to failure
      */
@@ -149,23 +206,77 @@ data class PollFailedEvent(
 }
 
 /**
+ * Event describing a failed poll.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+data class PollFailedEvent(
+    override val requestEvent: IPollRequestEvent,
+    override val exception: Exception,
+    override val failedTimestamp: Long) : IPollFailedEvent {
+
+    override val requestTime: Long
+        get() = failedTimestamp - requestEvent.requestTimestamp
+}
+
+/**
+ * Event describing a publication request.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+interface IPublicationRequestEvent {
+    /**
+     * @return the topic path
+     */
+    val path: String
+    /**
+     * @return the length of the value
+     */
+    val valueLength: Int
+    /**
+     * @return the request timestamp
+     */
+    val requestTimestamp: Long
+}
+
+/**
  * Event describing a publication request.
  *
  * @author Matt Champion 17/05/2017
  */
 data class PublicationRequestEvent(
-        /**
-         * @return the topic path
-         */
-        val path: String,
-        /**
-         * @return the length of the value
-         */
-        val valueLength: Int,
-        /**
-         * @return the request timestamp
-         */
-        val requestTimestamp: Long)
+    /**
+     * @return the topic path
+     */
+    override val path: String,
+    /**
+     * @return the length of the value
+     */
+    override val valueLength: Int,
+    /**
+     * @return the request timestamp
+     */
+    override val requestTimestamp: Long) : IPublicationRequestEvent
+
+/**
+ * Event describing a successful publication.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+interface IPublicationSuccessEvent {
+    /**
+     * @return the publication request event
+     */
+    val requestEvent: IPublicationRequestEvent
+    /**
+     * @return the success timestamp
+     */
+    val successTimestamp: Long
+    /**
+     * @return the request timestamp
+     */
+    val requestTime: Long
+}
 
 /**
  * Event describing a successful publication.
@@ -173,19 +284,10 @@ data class PublicationRequestEvent(
  * @author Matt Champion 17/05/2017
  */
 data class PublicationSuccessEvent(
-        /**
-         * @return the publication request event
-         */
-        val requestEvent: PublicationRequestEvent,
-        /**
-         * @return the success timestamp
-         */
-        val successTimestamp: Long) {
+    override val requestEvent: IPublicationRequestEvent,
+    override val successTimestamp: Long) : IPublicationSuccessEvent {
 
-    /**
-     * @return the request timestamp
-     */
-    val requestTime: Long
+    override val requestTime: Long
         get() = successTimestamp - requestEvent.requestTimestamp
 }
 
@@ -194,23 +296,36 @@ data class PublicationSuccessEvent(
  *
  * @author Matt Champion 17/05/2017
  */
-data class PublicationFailedEvent(
-        /**
-         * @return the publication request event
-         */
-        val requestEvent: PublicationRequestEvent,
-        /**
-         * @return the error reason
-         */
-        val errorReason: ErrorReason,
-        /**
-         * @return the failure timestamp
-         */
-        val failedTimestamp: Long) {
-
+interface IPublicationFailedEvent {
+    /**
+     * @return the publication request event
+     */
+    val requestEvent: IPublicationRequestEvent
+    /**
+     * @return the error reason
+     */
+    val errorReason: ErrorReason
+    /**
+     * @return the failure timestamp
+     */
+    val failedTimestamp: Long
     /**
      * @return the time to failure
      */
     val requestTime: Long
+        get() = failedTimestamp - requestEvent.requestTimestamp
+}
+
+/**
+ * Event describing a failed publication.
+ *
+ * @author Matt Champion 17/05/2017
+ */
+data class PublicationFailedEvent(
+    override val requestEvent: IPublicationRequestEvent,
+    override val errorReason: ErrorReason,
+    override val failedTimestamp: Long) : IPublicationFailedEvent {
+
+    override val requestTime: Long
         get() = failedTimestamp - requestEvent.requestTimestamp
 }
