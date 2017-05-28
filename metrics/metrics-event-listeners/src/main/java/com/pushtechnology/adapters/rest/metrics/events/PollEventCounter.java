2c4e111ece079c31262b015cc395abd5e53e5d25
Matt Champion <mattunderscorechampion@gmail.com>
2017-05-28 17:29:16 +0100

    Add an implementation for counting poll events.

---
+++ metrics/metrics-event-listeners/src/main/java/com/pushtechnology/adapters/rest/metrics/events/PollEventCounter.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.metrics.events;
+
+import org.apache.http.HttpResponse;
+
+import com.pushtechnology.adapters.rest.metrics.PollListener;
+import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+
+/**
+ * Implementation of {@link EventCounter} for poll events.
+ *
+ * @author Matt Champion 28/05/2017
+ */
+public final class PollEventCounter extends AbstractEventCounter implements PollListener {
+    private final PollCompletionListener completionListener = new PollCompletionListener() {
+        @Override
+        public void onPollResponse(HttpResponse response) {
+            onSuccess();
+        }
+
+        @Override
+        public void onPollFailure(Exception exception) {
+            onFailure();
+        }
+    };
+
+    @Override
+    public PollCompletionListener onPollRequest(ServiceConfig serviceConfig, EndpointConfig endpointConfig) {
+        onRequest();
+        return completionListener;
+    }
+}
