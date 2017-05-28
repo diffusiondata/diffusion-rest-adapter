fe377b9c803bbc07bf87259df7432a334e2d6a6e
Matt Champion <mattunderscorechampion@gmail.com>
2017-05-28 17:33:25 +0100

    Add an implementation for counting publication events.

---
+++ metrics/metrics-event-listeners/src/main/java/com/pushtechnology/adapters/rest/metrics/events/PublicationEventCounter.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.metrics.events;
+
+import com.pushtechnology.adapters.rest.metrics.PublicationListener;
+import com.pushtechnology.adapters.rest.model.latest.EndpointConfig;
+import com.pushtechnology.adapters.rest.model.latest.ServiceConfig;
+import com.pushtechnology.diffusion.client.callbacks.ErrorReason;
+import com.pushtechnology.diffusion.datatype.Bytes;
+
+/**
+ * Implementation of {@link EventCounter} for publication events.
+ *
+ * @author Matt Champion 28/05/2017
+ */
+public final class PublicationEventCounter extends AbstractEventCounter implements PublicationListener {
+    private final PublicationCompletionListener completionListener = new PublicationCompletionListener() {
+        @Override
+        public void onPublication() {
+            onSuccess();
+        }
+
+        @Override
+        public void onPublicationFailed(ErrorReason reason) {
+            onFailure();
+        }
+    };
+
+    @Override
+    public PublicationCompletionListener onPublicationRequest(
+            ServiceConfig serviceConfig,
+            EndpointConfig endpointConfig,
+            Bytes value) {
+
+        onRequest();
+        return completionListener;
+    }
+}
---
+++ metrics/metrics-event-listeners/src/test/java/com/pushtechnology/adapters/rest/metrics/events/PublicationEventCounterTest.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.adapters.rest.metrics.events;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link PublicationEventCounter}.
+ *
+ * @author Matt Champion 28/05/2017
+ */
+public final class PublicationEventCounterTest {
+    @Test
+    public void onRequest() throws Exception {
+        final PublicationEventCounter counter = new PublicationEventCounter();
+
+        counter.onPublicationRequest(null, null, null);
+
+        assertEquals(1, counter.getRequests());
+        assertEquals(0, counter.getSuccesses());
+        assertEquals(0, counter.getFailures());
+    }
+
+    @Test
+    public void onSuccess() throws Exception {
+        final PublicationEventCounter counter = new PublicationEventCounter();
+
+        counter.onPublicationRequest(null, null, null).onPublication();
+
+        assertEquals(1, counter.getRequests());
+        assertEquals(1, counter.getSuccesses());
+        assertEquals(0, counter.getFailures());
+    }
+
+    @Test
+    public void onFailure() throws Exception {
+        final PublicationEventCounter counter = new PublicationEventCounter();
+
+        counter.onPublicationRequest(null, null, null).onPublicationFailed(null);
+
+        assertEquals(1, counter.getRequests());
+        assertEquals(0, counter.getSuccesses());
+        assertEquals(1, counter.getFailures());
+    }
+}
