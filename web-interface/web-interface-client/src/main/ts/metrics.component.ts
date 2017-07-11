0c204d88febac297e50721f78918ddaed067af3c
Matt Champion <mattunderscorechampion@gmail.com>
2017-07-11 23:07:53 +0100

    Add an example metrics timeline to the web interface.

--- web-interface/web-interface-client/src/main/ts/metrics.component.ts
+++ web-interface/web-interface-client/src/main/ts/metrics.component.ts
@@ -1,5 +1,5 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'metrics',
@@ -10,9 +10,29 @@ import { Component } from '@angular/core';
             <h3 class="panel-title">Metrics</h3>
         </div>
         <div class="panel-body">
+            <div id="visualization"></div>
         </div>
     </div>
 </div>`
 })
-export class MetricsComponent {
+export class MetricsComponent implements OnInit {
+
+    ngOnInit(): void {
+        // DOM element where the Timeline will be attached
+        var container = document.getElementById('visualization');
+
+        // Create a DataSet (allows two way data-binding)
+        var items = new vis.DataSet([
+            {id: 1, content: 'www.example.org/hello', className: 'poll-event', start: new Date(2017, 7, 11, 0, 0, 0, 0)},
+            {id: 2, content: 'example/data', className: 'publish-event', start: new Date(2017, 7, 11, 0, 0, 1, 0)}
+        ]);
+
+        // Configuration for the Timeline
+        var options = {
+            selectable: false
+        };
+
+        // Create a Timeline
+        var timeline = new vis.Timeline(container, items, options);
+    }
 }
