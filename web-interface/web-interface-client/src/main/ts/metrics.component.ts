2963816dd23b1d81209dad9a4bcee87b90bca8f9
Matt Champion <mattunderscorechampion@gmail.com>
2017-12-02 22:08:05 +0000

    Fit the metrics into a smaller area.

--- web-interface/web-interface-client/src/main/ts/metrics.component.ts
+++ web-interface/web-interface-client/src/main/ts/metrics.component.ts
@@ -12,25 +12,25 @@ import {MetricsService, MetricsView} from "./metrics.service";
         </div>
         <div class="panel-body">
             <div class="form-horizontal">
-                <div class="form-group">
-                    <label for="url" class="col-sm-2 control-label">Request throughput</label>
-                    <p id="url" class="form-control-static col-sm-4">{{view.pollMetrics.requestThroughput}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="poll-request" class="col-sm-6 control-label">Request throughput</label>
+                    <p id="poll-request" class="form-control-static col-sm-6">{{view.pollMetrics.requestThroughput}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Failure throughput</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.pollMetrics.failureThroughput}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="poll-failure" class="col-sm-6 control-label">Failure throughput</label>
+                    <p id="poll-failure" class="form-control-static col-sm-6">{{view.pollMetrics.failureThroughput}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Max. successful request time</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.pollMetrics.maximumSuccessfulRequestTime}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="poll-max" class="col-sm-6 control-label">Max. successful request time</label>
+                    <p id="poll-max" class="form-control-static col-sm-6">{{view.pollMetrics.maximumSuccessfulRequestTime}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Min. successful request time</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.pollMetrics.minimumSuccessfulRequestTime}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="poll-min" class="col-sm-6 control-label">Min. successful request time</label>
+                    <p id="poll-min" class="form-control-static col-sm-6">{{view.pollMetrics.minimumSuccessfulRequestTime}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Successful request time 90th percentile</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.pollMetrics.successfulRequestTimeNinetiethPercentile}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="poll-90" class="col-sm-6 control-label">Successful request time 90th percentile</label>
+                    <p id="poll-90" class="form-control-static col-sm-6">{{view.pollMetrics.successfulRequestTimeNinetiethPercentile}}</p>
                 </div>
             </div>
         </div>
@@ -41,25 +41,25 @@ import {MetricsService, MetricsView} from "./metrics.service";
         </div>
         <div class="panel-body">
             <div class="form-horizontal">
-                <div class="form-group">
-                    <label for="url" class="col-sm-2 control-label">Request throughput</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.publicationMetrics.requestThroughput}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-request" class="col-sm-6 control-label">Request throughput</label>
+                    <p id="publication-request" class="form-control-static col-sm-6">{{view.publicationMetrics.requestThroughput}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Failure throughput</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.publicationMetrics.failureThroughput}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-failure" class="col-sm-6 control-label">Failure throughput</label>
+                    <p id="publication-failure" class="form-control-static col-sm-6">{{view.publicationMetrics.failureThroughput}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Max. successful request time</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.publicationMetrics.maximumSuccessfulRequestTime}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-max" class="col-sm-6 control-label">Max. successful request time</label>
+                    <p id="publication-max" class="form-control-static col-sm-6">{{view.publicationMetrics.maximumSuccessfulRequestTime}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Min. successful request time</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.publicationMetrics.minimumSuccessfulRequestTime}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-min" class="col-sm-6 control-label">Min. successful request time</label>
+                    <p id="publication-min" class="form-control-static col-sm-6">{{view.publicationMetrics.minimumSuccessfulRequestTime}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Successful request time 90th percentile</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.publicationMetrics.successfulRequestTimeNinetiethPercentile}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-90" class="col-sm-6 control-label">Successful request time 90th percentile</label>
+                    <p id="publication-90" class="form-control-static col-sm-6">{{view.publicationMetrics.successfulRequestTimeNinetiethPercentile}}</p>
                 </div>
             </div>
         </div>
@@ -70,25 +70,25 @@ import {MetricsService, MetricsView} from "./metrics.service";
         </div>
         <div class="panel-body">
             <div class="form-horizontal">
-                <div class="form-group">
-                    <label for="url" class="col-sm-2 control-label">Request throughput</label>
-                    <p id="url" class="form-control-static col-sm-4">{{view.topicCreationMetrics.requestThroughput}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-request" class="col-sm-6 control-label">Request throughput</label>
+                    <p id="publication-request" class="form-control-static col-sm-6">{{view.topicCreationMetrics.requestThroughput}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Failure throughput</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.topicCreationMetrics.failureThroughput}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-failure" class="col-sm-6 control-label">Failure throughput</label>
+                    <p id="publication-failure" class="form-control-static col-sm-6">{{view.topicCreationMetrics.failureThroughput}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Max. successful request time</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.topicCreationMetrics.maximumSuccessfulRequestTime}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-max" class="col-sm-6 control-label">Max. successful request time</label>
+                    <p id="publication-max" class="form-control-static col-sm-6">{{view.topicCreationMetrics.maximumSuccessfulRequestTime}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Min. successful request time</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.topicCreationMetrics.minimumSuccessfulRequestTime}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-min" class="col-sm-6 control-label">Min. successful request time</label>
+                    <p id="publication-min" class="form-control-static col-sm-6">{{view.topicCreationMetrics.minimumSuccessfulRequestTime}}</p>
                 </div>
-                <div class="form-group">
-                    <label for="topicPath" class="col-sm-2 control-label">Successful request time 90th percentile</label>
-                    <p id="topicPath" class="form-control-static col-sm-4">{{view.topicCreationMetrics.successfulRequestTimeNinetiethPercentile}}</p>
+                <div class="form-group col-sm-4">
+                    <label for="publication-90" class="col-sm-6 control-label">Successful request time 90th percentile</label>
+                    <p id="publication-90" class="form-control-static col-sm-6">{{view.topicCreationMetrics.successfulRequestTimeNinetiethPercentile}}</p>
                 </div>
             </div>
         </div>
