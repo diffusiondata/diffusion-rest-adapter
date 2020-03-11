edd0d75eb9cfebe90fc8b1fd09fa48523fbe382e
Matt Champion <mattunderscorechampion@gmail.com>
2020-03-11 23:01:18 +0000

    Bootstrap Angular using TypeScript code.

--- web-interface/web-interface-client/src/main/ts/init.js
+++
@@ -1,9 +0,0 @@
-
-(function() {
-    // TODO: Bootstrap TypScript without JavaScript
-    // Pull in Angular 2 dependencies
-    var reflect = require('reflect-metadata');
-    var zone = require('zone.js');
-    // Pull in Angular 2 application
-    var main = require('./main');
-})();
---
+++ web-interface/web-interface-client/src/main/ts/init.ts
@@ -0,0 +1,8 @@
+
+(function() {
+    // Pull in Angular dependencies
+    const reflect = require('reflect-metadata');
+    const zone = require('zone.js');
+    // Pull in Angular 2 application
+    const main = require('./main');
+})();
